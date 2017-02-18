b9edd622135bed568bf67ce678a440c439f104d8
Anton Boyko <v-anboyk@microsoft.com>
2017-02-18 12:32:08 +0200

    meetup odata api key added

--- services/configuration.js
+++ services/configuration.js
@@ -6,6 +6,7 @@ nconf
 	.defaults({
 		'ODataApiEndpoint' : 'http://meetup-api-odata-p6g7a5qcxvlna.azurewebsites.net/odata/v1',
 		'ODataApiMetadataEndpoint': 'http://meetup-api-odata-p6g7a5qcxvlna.azurewebsites.net/swagger/docs/v1',
+		'ODataApiKey': '',
 		'StorageApiEndpoint': '',
 		'Environment': 'dev'
 	});
@@ -13,6 +14,7 @@ nconf
 var configurationData = {};
 
 configurationData.ODataApiEndpoint = nconf.get('ODataApiEndpoint');
+configurationData.ODataApiKey = nconf.get('ODataApiKey');
 configurationData.ODataApiMetadataEndpoint = nconf.get('ODataApiMetadataEndpoint');
 configurationData.StorageApiEndpoint = nconf.get('StorageApiEndpoint');
 configurationData.Environment = nconf.get('Environment');
